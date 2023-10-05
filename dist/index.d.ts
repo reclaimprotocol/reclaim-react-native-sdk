0363133f2c9fdeda0ea907e1287640d8cc98f0fa
akshaynarisetti <akshaynarisettia@gmail.com>
2023-10-05 21:38:59 +0530

    Added Types

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+import ReclaimHttps from './ReclaimHttps';
+import ReclaimAadhaar from './ReclaimAadhaar';
+import ReclaimKaggle from './ReclaimKaggle';
+export { ReclaimHttps, ReclaimAadhaar, ReclaimKaggle };
---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ReclaimKaggle = exports.ReclaimAadhaar = exports.ReclaimHttps = void 0;
+const ReclaimHttps_1 = __importDefault(require("./ReclaimHttps"));
+exports.ReclaimHttps = ReclaimHttps_1.default;
+const ReclaimAadhaar_1 = __importDefault(require("./ReclaimAadhaar"));
+exports.ReclaimAadhaar = ReclaimAadhaar_1.default;
+const ReclaimKaggle_1 = __importDefault(require("./ReclaimKaggle"));
+exports.ReclaimKaggle = ReclaimKaggle_1.default;
