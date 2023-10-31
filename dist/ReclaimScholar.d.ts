ae62395879cc51c8edb91134706da4937e9dd662
Y5Yash <30973335+Y5Yash@users.noreply.github.com>
2023-10-31 19:32:38 +0530

    Add google scholar citations count provider (#6)
    * add first commit for google scholar
    
    * add build files
    
    * update index.ts to get exports
    
    * add comments temporarily
    
    * add more comments, minor fixes
    
    * remove debug comments

---
+++ dist/ReclaimScholar.d.ts
@@ -0,0 +1,11 @@
+import * as React from "react";
+type Props = {
+    title: string;
+    subTitle: string;
+    cta: string;
+    onSuccess: (proofs: any[]) => void;
+    onFail: (e: Error) => void;
+    context?: string;
+};
+export default function ReclaimScholar({ title, subTitle, cta, onSuccess, onFail, }: Props): React.JSX.Element;
+export {};
